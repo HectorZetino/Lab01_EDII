ac39dd678208ce85699a110524a469155eeeb190
HectorZetino <h.rzc.zetino@hotmail.es>
2020-09-04 23:02:33 -0600

    Agregando biblioteca de clases

---
+++ Lab01_EDII/Arbolmulticamino/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Arbol_Multicamino;
+
+namespace Arbolmulticamino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArbolMulticamino<int> arbolito;
+            arbolito = new ArbolMulticamino<int>(3);
+            for (int i = 0; i <= 2; i++)
+            {
+                arbolito.AgregarNodo(i);
+            }
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
