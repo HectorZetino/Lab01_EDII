7d888a89bf8aa4637bc51e46d9b9d12b9a4b4608
Gerardo0112 <gerar0112@gmail.com>
2020-09-08 19:09:30 -0600

    Clases de manejo de datos y busqueda

---
+++ Lab01_EDII/Lab01_EDII/Busqueda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab01_EDII
+{
+    public class Busqueda
+    {
+        //Permite principalmente una busqueda o manejo de informacion predeterminado por el genero.
+        public int Find(Pelicula peli, Pelicula peli2)
+        {
+            return peli == null || peli2 == null ? 1 : peli.genre.CompareTo(peli2.genre);
+        }
+    }
+}
---
+++ Lab01_EDII/Lab01_EDII/Datos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab01_EDII
+{
+    public class Datos
+    {
+        //Permite controlar y manejar la conversacion de datos en el arbol.
+        private static Datos d = null;
+        private static Busqueda b = new Busqueda();
+
+        public static Datos data
+        {
+            get
+            {
+                if (d == null) d = new Datos();
+                return d;
+            }
+        }
+    }
+}
