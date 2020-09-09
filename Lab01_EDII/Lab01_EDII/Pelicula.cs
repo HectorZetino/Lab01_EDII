1eecf7adb939782a2d5e85b7c041c43a333e17d2
Gerardo0112 <gerar0112@gmail.com>
2020-09-08 18:08:48 -0600

    Modelo pelicula

---
+++ Lab01_EDII/Lab01_EDII/Pelicula.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab01_EDII
+{
+    public class Pelicula
+    {
+        public string director { get; set; }
+        public double imdbRating { get; set; }
+        public string genre { get; set; }
+        public string releaseDate { get; set; }
+        public int rottenTomatoesRating { get; set; }
+        public string title { get; set; }
+
+        public Pelicula(string D, double I, string G, string RD, int RTR, string T) 
+        {
+            D = director;
+            I = imdbRating;
+            G = genre;
+            RD = releaseDate;
+            RTR = rottenTomatoesRating;
+            T = title;
+        }
+    }
+}
